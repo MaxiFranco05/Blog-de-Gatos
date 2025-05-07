9097bff82416811c17821f89ab3572262d9419da
Maxi <maxifranco1011@gmail.com>
2025-05-07 18:56:09 -0300

    v0.6(db_config_4)

--- app/core/database.py
+++ app/core/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "postgresql://postgres:ZBa9moa6i2lBj3Xq@db.gpnpykpetfthniswqgsk.supabase.co:5432/blog-de-gatos"
+DATABASE_URL = "postgresql://postgres:ZBa9moa6i2lBj3Xq@db.gpnpykpetfthniswqgsk.supabase.co:5432/Blog-of-cats"
 
 engine = create_engine(DATABASE_URL)
 
