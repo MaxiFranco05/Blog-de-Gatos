f6aed0a1ccb6b593e0e9955bcc88bead2f74f5bd
Maxi <maxifranco1011@gmail.com>
2025-05-16 18:45:32 -0300

    add CORS v1.0

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from app.modules.post.route import router as post_route
 from app.modules.users.route import router as user_route
+from fastapi.middleware.cors import CORSMiddleware
 
 tags_metadata = [
     {
@@ -18,6 +19,14 @@ tags_metadata = [
 ]
 
 app = FastAPI(title="Blog de Gatos",description="API para manejar un blog con FastAPI", version="1.0.0", openapi_tags=tags_metadata)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://127.0.0.1:5500"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(post_route)
 app.include_router(user_route)
